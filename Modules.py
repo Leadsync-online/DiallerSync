9740bc29d34e0d401fc9ff7d906c319ba397c079
Leadsync-online <admin@leadsynconline.com>
2025-01-11 03:20:45 +0200

    Update Modules.py

--- Modules.py
+++ Modules.py
@@ -1,2 +1,10 @@
 import streamlit as st
 from supabase import create_client, Client
+
+def login(username, password):
+    try:
+        user = supabase.auth.sign_in_with_password({"email": username, "password": password})
+        return user
+    except Exception:
+        st.error("Login failed. Check your username and password.")
+        return None
