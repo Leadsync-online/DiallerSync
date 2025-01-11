fd23eace6765608210f0b243180d09d698bdaf75
Leadsync-online <admin@leadsynconline.com>
2025-01-12 00:54:16 +0200

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -2,4 +2,17 @@ import streamlit as st
 from supabase import create_client, Client
 import Modules as md
 
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"]{
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
 st.header("Dashboard")
