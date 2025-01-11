d63602480369f1ccc00b0aea48b2d686769cbe2f
Leadsync-online <admin@leadsynconline.com>
2025-01-11 03:22:31 +0200

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from supabase import create_client, Client
 
-# Initialize Supabase client
-SUPABASE_URL = "https://adqhjaqlidqbiqltwhpk.supabase.co"  # Replace with your Supabase project URL
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcWhqYXFsaWRxYmlxbHR3aHBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzMjc3MDgsImV4cCI6MjA0NDkwMzcwOH0.O3MVZ-FTnR43E6GYgguGn_hl3uWrv6bu3pYkVLIrNkI"  # Replace with your Supabase project API key
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 # Hide Streamlit sidebar and other UI elements
 hide_st_style = """
     <style>
