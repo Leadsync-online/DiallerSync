fc288a2beaab6c763b0985979290e1275621bf36
Leadsync-online <admin@leadsynconline.com>
2025-01-11 02:53:59 +0200

    Update Signup.py

--- pages/Signup.py
+++ pages/Signup.py
@@ -1,1 +1,13 @@
+# Sign-up page
+def signup_page():
+    st.title("Sign Up")
+    st.write("Create a new account.")
 
+    signup_email = st.text_input("New Email", key="signup_email")
+    signup_password = st.text_input("New Password", type="password", key="signup_password")
+    if st.button("Create Account"):
+        signup(signup_email, signup_password)
+
+    # Back to login button
+    if st.button("Back to Login"):
+        st.switch_page("Login")
