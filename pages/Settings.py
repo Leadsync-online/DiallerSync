ad82ce402561b5cc3e8b6bce2cd9c84b1aaede58
Leadsync-online <admin@leadsynconline.com>
2025-01-12 03:43:36 +0200

    Update Settings.py

--- pages/Settings.py
+++ pages/Settings.py
@@ -24,3 +24,8 @@ col1, col2 = st.columns([1,1])
 
 with col1:
     st.header("Settings")
+
+    if st.button("Dashboard", use_container_width=True):
+    st.switch_page("pages/Home.py")
+    if st.button("Logout", use_container_width=True):
+    st.switch_page("pages/Login.py")
