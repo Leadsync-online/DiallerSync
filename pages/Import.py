befa169345e2dca0395d26ba363e5dd7e9987ae5
Leadsync-online <admin@leadsynconline.com>
2025-01-12 03:31:44 +0200

    Update Import.py

--- pages/Import.py
+++ pages/Import.py
@@ -29,3 +29,6 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
+
+if st.button("Dashboard", use_container_width=True):
+    st.switch_page("pages/Home.py")
