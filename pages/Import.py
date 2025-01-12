80a98b7cc8411e3c4a4d4ac8152e3ae917bbee14
Leadsync-online <admin@leadsynconline.com>
2025-01-12 03:30:41 +0200

    Update Import.py

--- pages/Import.py
+++ pages/Import.py
@@ -1,1 +1,31 @@
+import streamlit as st
+from supabase import create_client, Client
+import Modules as md
+from datetime import date, timedelta
+import pandas as pd
+import numpy as np
 
+today = date.today()
+
+st.set_page_config(initial_sidebar_state="collapsed",layout="wide")
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"]{
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+st.header("Import new file")
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
