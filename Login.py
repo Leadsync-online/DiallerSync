70ce148057d1690efacba3bf49e5a8d6397179a9
Leadsync-online <admin@leadsynconline.com>
2025-02-26 00:46:01 +0200

    Update Login.py

--- Login.py
+++ Login.py
@@ -8,7 +8,7 @@ st.set_page_config(initial_sidebar_state="collapsed")
 st.markdown(
     """
 <style>
-    [data-testid="viewBox"] {
+    [data-testid="stBaseButton"] {
         display: none
     }
 </style>
