922e036565da75f5fdd31e59c1d225ca496ffcfb
Leadsync-online <admin@leadsynconline.com>
2025-02-26 22:00:45 +0200

    Update Modules.py

--- Modules.py
+++ Modules.py
@@ -4,7 +4,6 @@ import pandas as pd
 import csv
 import os
 
-
 # Initialize Supabase client
 SUPABASE_URL = "https://adqhjaqlidqbiqltwhpk.supabase.co"  # Replace with your Supabase project URL
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcWhqYXFsaWRxYmlxbHR3aHBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzMjc3MDgsImV4cCI6MjA0NDkwMzcwOH0.O3MVZ-FTnR43E6GYgguGn_hl3uWrv6bu3pYkVLIrNkI"  # Replace with your Supabase project API key
@@ -28,3 +27,23 @@ def signup(username, password):
     except Exception as e:
         st.error(f"Sign-up failed: {e}")
 
+# File checker
+def detect_delimiter(file_path):
+    with open(file_path, 'r', newline='', encoding='utf-8') as file:
+        sample = file.read(1024)
+        sniffer = csv.Sniffer()
+        delimiter = sniffer.sniff(sample).delimiter
+        return delimiter
+
+def read_file(file_path):
+    ext = os.path.splitext(file_path)[-1].lower()
+    
+    if ext in ['.xls', '.xlsx']:
+        df = pd.read_excel(file_path)
+    elif ext in ['.csv', '.txt']:
+        delimiter = detect_delimiter(file_path)
+        df = pd.read_csv(file_path, delimiter=delimiter)
+    else:
+        raise ValueError("Unsupported file format. Please provide a CSV, TXT, or Excel file.")
+    
+    return df
