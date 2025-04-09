aec29cd73212e624418db30f85ccf63b5bfd7d66
Leadsync-online <admin@leadsynconline.com>
2025-04-09 21:45:12 +0200

    Update NewCompany.py

--- pages/NewCompany.py
+++ pages/NewCompany.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from supabase import create_client, Client
+import Modules as md
+import time
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+st.markdown(
+    """
+<style>
+    [data-testid="stSidebarCollapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
